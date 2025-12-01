d654f6cb3c6722562ad9f88adb0fdcc3c8d63944
colasa-hub <a13222326075@126.com>
2025-12-01 15:16:36 +0800

    URL：https://www.imooc.com/video/23501

---
+++ project 07 (Spring Boot Basics)/src/main/java/com/imooc/mapper/DbStuMapper.java
@@ -0,0 +1,7 @@
+package com.imooc.mapper;
+
+import com.imooc.my_mapper.MyMapper;
+import com.imooc.pojo.DbStu;
+
+public interface DbStuMapper extends MyMapper<DbStu> {
+}
---
+++ project 07 (Spring Boot Basics)/src/main/java/com/imooc/mapper/StuMapper.java
@@ -0,0 +1,7 @@
+package com.imooc.mapper;
+
+import com.imooc.my_mapper.MyMapper;
+import com.imooc.pojo.Stu;
+
+public interface StuMapper extends MyMapper<Stu> {
+}
